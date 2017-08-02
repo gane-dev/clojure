137c78c15b8c7a32e324b06ba0a4071a85c40a5e
Devaraj <Ganesh.Devaraj@avendra.com>
2017-08-02 12:31:33 -0400

    intro om next

---
+++ ganesh-om-intro/src/ganesh-om-intro/core.cljs
@@ -0,0 +1,26 @@
+(ns ganesh-om-intro.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+(def app-state (atom {:count 0}))
+
+(defui Counter
+  Object
+  (render [this]
+          (let [{:keys [count]} (om/props this)]
+            (dom/div nil
+                     (dom/span nil (str "Count: " count))
+                     (dom/button
+                       #js {:onClick
+                            (fn [e]
+                              (println "Count: " count)
+                              (swap! app-state update-in [:count] inc))}
+                       "Click me!")))))
+
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler
+              Counter (gdom/getElement "app"))
